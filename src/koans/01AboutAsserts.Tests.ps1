4aa82dfd7973c770935808b74a3e178574e72f2e
Peter Bishop <pebishop@wayfair.com>
2018-09-26 03:49:08 -0400

    meditated on 01AboutAsserts

--- src/koans/01AboutAsserts.Tests.ps1
+++ src/koans/01AboutAsserts.Tests.ps1
@@ -1,17 +1,17 @@
 Describe "AboutAsserts" {
 	It "should expect true" {
 		# We shall contemplate truth by testing reality, via asserts.
-		$false | Should Be $true # This should be true
+		$true | Should Be $true # This should be true
 	}
 	
 	It "should fill in values" {
 		# Sometimes we will ask you to fill in the values
-		(1 + 1) | Should Be $__FILL_ME_IN__
+		(1 + 1) | Should Be 2
 	}
 	
 	It "should assert one's expectations" {
 		# Some ways of asserting equality are better than others
-		$expected = $__FILL_ME_IN__
+		$expected = 2
 		$actual = 1 + 1
 		$actual | Should Be $expected
 	}
